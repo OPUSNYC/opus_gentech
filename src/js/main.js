2eef63eea347a935ef6fc324415c80ed07912b49
jwoos <shin.junwoo97@gmail.com>
2014-08-07 01:08:37 -0400

    added color option for tabbar and topbar; set image title for top bar

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,16 @@
 forge.logging.info("LOGGING INFO");
 
+//choose colors for tabbar and topbar
+var color1 = [0,108,204,255];
+var color2 = [0,0,0,255];
+
 //START TOPBAR CONFIGURATION
 forge.topbar.setTitle("OPUS");
+//forge.topbar.setTitleImage("images/logo strong.png");
+forge.topbar.setTint(color1);
 
 //START TABBAR CONFIGURATION
-array = ["0,0,255,255"]
-forge.tabbar.setTint(array);
-
-forge.tabbar.addButton({
-  icon: "images/prof_icon.png",
-  text: "Profile"
-}, function (button) {
-  button.onPressed.addListener(function () {
-    window.location="studentprofile.html";
-	forge.tabbar.removeButtons();
-	forge.topbar.removeButtons();
-  });
-});
+forge.tabbar.setTint(color2);
 
 forge.tabbar.addButton({
   icon: "images/job_icon.png",
@@ -24,6 +18,17 @@ forge.tabbar.addButton({
 }, function (button) {
   button.onPressed.addListener(function () {
     window.location="jobs.html";
+	forge.tabbar.removeButtons();
+	forge.topbar.removeButtons();
+  });
+});
+
+forge.tabbar.addButton({
+  icon: "images/prof_icon.png",
+  text: "Profile"
+}, function (button) {
+  button.onPressed.addListener(function () {
+    window.location="studentprofile.html";
 	forge.tabbar.removeButtons();
 	forge.topbar.removeButtons();
   });
