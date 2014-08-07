677d86c4e324db4452623dc241862cc96ce69011
jwoos <shin.junwoo97@gmail.com>
2014-08-07 00:39:44 -0400

    added tab bar and topbar, mostly works crashes when going back from maps (needs local cache); need to change the tint of tab/top bar

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,28 @@ forge.logging.info("LOGGING INFO");
 
 //START TOPBAR CONFIGURATION
 forge.topbar.setTitle("OPUS");
+
+//START TABBAR CONFIGURATION
+forge.tabbar.setTint("[255, 0, 0, 255]");
+
+forge.tabbar.addButton({
+  icon: "images/prof_icon.png",
+  text: "Profile"
+}, function (button) {
+  button.onPressed.addListener(function () {
+    window.location="studentprofile.html";
+	forge.tabbar.removeButtons();
+	forge.topbar.removeButtons();
+  });
+});
+
+forge.tabbar.addButton({
+  icon: "images/job_icon.png",
+  text: "Jobs"
+}, function (button) {
+  button.onPressed.addListener(function () {
+    window.location="jobs.html";
+	forge.tabbar.removeButtons();
+	forge.topbar.removeButtons();
+  });
+});
